e3af118a0a6638bcd51054f45247c5cf966a91c5
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-28 13:59:47 +0300

    creating the add html template with the add dynamiv route

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,4 @@
-import contextlib
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Category,Photo
 
 # Create your views here.
@@ -16,4 +15,30 @@ def viewPhoto(request,pk):
     return render(request, 'photos/photo.html',{'photo':photo})
 
 def addPhoto(request):
-    return render(request, 'photos/add.html')
+    
+    categories =Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        images = request.FILES.getlist('images')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+
+                name=data['category_new'])
+        else:
+            category = None
+
+        for image in images:
+            photo = Photo.objects.create(
+                category=category,
+                description=data['description'],
+                image=image,
+            )
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
