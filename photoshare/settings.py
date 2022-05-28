7ed71ad86c67dd08366f1c825e8872d254a8ac7b
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-28 11:36:02 +0300

    creating the app database

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'photoshare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photos',
+        'USER': 'oscar',
+        'PASSWORD':'4321'
     }
 }
 
