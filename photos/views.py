5deaa2baddf26c55061ce41f5fd49f3e200d3e9c
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-28 14:56:16 +0300

    filtering photos by category

--- photos/views.py
+++ photos/views.py
@@ -4,8 +4,14 @@ from . models import Category,Photo
 # Create your views here.
 
 def gallery(request):
+    category =request.GET.get('category')
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)   
+    
+    
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     
     context = {'categories':categories,'photos':photos}
     return render(request, 'photos/gallery.html',context)
