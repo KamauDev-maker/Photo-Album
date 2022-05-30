eaf9c93e03d3f585937f148f5ae8771d8be7c591
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-30 12:16:23 +0300

    creating java script

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
-function copyclipboard(){
-    let copyText = document.getElementById('link')
-        copyText.select();
-        let imageUrl = window.location.href +copyText.value.slice(1)
-        navigator.clipboard.writeText(imageUrl)
-        alert("Copied to clipboard:"+ imageUrl)
+function copyclipboard(e){
+    const copyText = e.previousElementSibling
+    copyText.select()
+    let imageUrl = window.location.origin + "/" +copyText.value.slice(1)
+    navigator.clipboard.writeText(imageUrl)
+    alert("Copied clipboard:"+ imageUrl)
 }
