f8a121a1da244d6fbf71620a8cdb4428bd3e47e9
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-31 15:02:22 +0300

    rewritting the js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 function copyclipboard(e){
     const copyText = e.previousElementSibling
     copyText.select()
-    // let imageUrl = window.location.origin + "/" +copyText.value.slice(1)
     navigator.clipboard.writeText(copyText.value)
     alert("Copied clipboard :"+ copyText.value)
 }
